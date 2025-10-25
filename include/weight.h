312b5ec15bf420b526e3e0bb75387f7660300467
Полина <p.sinitsina.05@mail.ru>
2025-10-25 14:21:45 +0300

    change weight

--- include/weight.h
+++ include/weight.h
@@ -6,17 +6,14 @@
 
 class WeightConverter {
 public:
-    // Конвертация веса
+    
     static double convert(const std::string& fromUnit, const std::string& toUnit, double value);
 
-    // Список поддерживаемых единиц
     static std::string getSupportedUnits();
 
 private:
-    // Базовые конвертации в килограммы
     static double toKilograms(const std::string& unit, double value);
 
-    // Конвертация из килограммов в другие единицы
     static double fromKilograms(const std::string& unit, double value);
 };
 
--- src/weight.cpp
+++ src/weight.cpp
@@ -5,59 +5,48 @@
 using namespace std;
 
 double WeightConverter::convert(const string& fromUnit, const string& toUnit, double value) {
-
     double kilograms = toKilograms(fromUnit, value);
     return fromKilograms(toUnit, kilograms);
 }
 
 string WeightConverter::getSupportedUnits() {
-    return "kg (����������), g (������), lb (�����), oz (�����), t (�����)";
+    return "kg (килограммы), g (граммы), lb (фунты), oz (унции), t (тонны)";
 }
 
 double WeightConverter::toKilograms(const string& unit, double value) {
     string unitLower = unit;
     transform(unitLower.begin(), unitLower.end(), unitLower.begin(), ::tolower);
-
-    if (unitLower == "kg" || unitLower == "��") {
+    
+    if (unitLower == "kg" || unitLower == "кг") {
         return value;
-    }
-    else if (unitLower == "g" || unitLower == "�") {
+    } else if (unitLower == "g" || unitLower == "г") {
         return value / 1000.0;
-    }
-    else if (unitLower == "lb") {
+    } else if (unitLower == "lb") {
         return value * 0.453592;
-    }
-    else if (unitLower == "oz") {
+    } else if (unitLower == "oz") {
         return value * 0.0283495;
-    }
-    else if (unitLower == "t" || unitLower == "�") {
+    } else if (unitLower == "t" || unitLower == "т") {
         return value * 1000.0;
-    }
-    else {
-        throw invalid_argument("����������� ������� ����: " + unit);
+    } else {
+        throw invalid_argument("Неизвестная единица веса: " + unit);
     }
 }
 
 double WeightConverter::fromKilograms(const string& unit, double value) {
     string unitLower = unit;
     transform(unitLower.begin(), unitLower.end(), unitLower.begin(), ::tolower);
-
-    if (unitLower == "kg" || unitLower == "��") {
+    
+    if (unitLower == "kg" || unitLower == "кг") {
         return value;
-    }
-    else if (unitLower == "g" || unitLower == "�") {
+    } else if (unitLower == "g" || unitLower == "г") {
         return value * 1000.0;
-    }
-    else if (unitLower == "lb") {
+    } else if (unitLower == "lb") {
         return value / 0.453592;
-    }
-    else if (unitLower == "oz") {
+    } else if (unitLower == "oz") {
         return value / 0.0283495;
-    }
-    else if (unitLower == "t" || unitLower == "�") {
+    } else if (unitLower == "t" || unitLower == "т") {
         return value / 1000.0;
-    }
-    else {
-        throw invalid_argument("����������� ������� ����: " + unit);
+    } else {
+        throw invalid_argument("Неизвестная единица веса: " + unit);
     }
 }
